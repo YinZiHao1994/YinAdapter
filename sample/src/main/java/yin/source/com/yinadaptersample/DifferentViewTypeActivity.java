13c3e2394109326aed7037f726cf966a03e2d6f9
YinZiHao1994 <1035909389@qq.com>
2017-04-27 18:53:02 +0800

    新增分组栏封装类

--- sample/src/main/java/yin/source/com/yinadaptersample/DifferentViewTypeActivity.java
+++ sample/src/main/java/yin/source/com/yinadaptersample/DifferentViewTypeActivity.java
@@ -1,14 +1,17 @@
 package yin.source.com.yinadaptersample;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import yin.source.com.yinadapter.BaseAdapter;
-import yin.source.com.yinadapter.CommonViewHolder;
-import yin.source.com.yinadapter.DataType;
+import java.util.List;
+
+import yin.source.com.yinadapter.SectionDecoration;
 import yin.source.com.yinadaptersample.adapter.DifferentViewTypeAdapter;
 import yin.source.com.yinadaptersample.bean.PersonBean;
 
@@ -20,6 +23,8 @@ public class DifferentViewTypeActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
+    private List<PersonBean> personBeanList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +32,51 @@ public class DifferentViewTypeActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new DifferentViewTypeAdapter(this, PersonBean.init()));
+        personBeanList = PersonBean.init();
+        recyclerView.setAdapter(new DifferentViewTypeAdapter(this,personBeanList ));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+
+        SectionDecoration<Integer> sectionDecoration = new SectionDecoration<Integer>(this, new SectionDecoration.SectionCallback<Integer>() {
+            @Override
+            public Integer getSectionSign(int position) {
+                return personBeanList.get(position).getSex();
+            }
+
+            @Override
+            public String getSectionTitle(int position) {
+                return personBeanList.get(position).getName();
+            }
+        }, new SectionDecoration.ConfigureCallback() {
+            @Override
+            public Paint getTextPain() {
+                Paint textPaint = new Paint();
+                textPaint.setTextSize(50);
+                textPaint.setColor(Color.RED);
+                return textPaint;
+            }
+
+            @Override
+            public Paint getSectionBackgroundPaint() {
+                Paint backgroundPaint = new Paint();
+                backgroundPaint.setColor(Color.LTGRAY);
+                return backgroundPaint;
+            }
+
+            @Override
+            public int getSectionDecorationHeight() {
+                return 60;
+            }
+        });
+
+        Paint textPaint = new Paint();
+        textPaint.setTextSize(60);
+        textPaint.setColor(Color.RED);
+
+        sectionDecoration.setTextPaint(textPaint);
+        recyclerView.addItemDecoration(sectionDecoration);
+
+
 
     }
 
